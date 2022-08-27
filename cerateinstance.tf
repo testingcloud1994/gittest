043735edf7e3792ad8128f15f04bec796f8d61e2
testingcloud1994 <real.suraj1234@gmail.com>
2022-08-27 16:57:14 +0530

    dsf

--- cerateinstance.tf
+++ cerateinstance.tf
@@ -2,9 +2,6 @@ resource "google_compute_instance" "vm_instance" {
     count = 3
     name = "terraform_instance"
     machine_type = "e2-micro"
-    tags{ 
-      name="threedemoinstance"
-    }
     boot_disk {
       initialize_params{
         image= "ubuntu-os-cloud/ubuntu-2004-lts"
