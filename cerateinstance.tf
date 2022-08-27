b32ff5e6cf35f9681d1e19fe16ef3219de92a2a7
testingcloud1994 <real.suraj1234@gmail.com>
2022-08-27 16:22:45 +0530

    surah

--- cerateinstance.tf
+++ cerateinstance.tf
@@ -15,7 +15,7 @@ resource "google_compute_instance" "vm_instance" {
       }
       }
 network_interface {
-    network= "mytestnetwork"
+    network= "default"
     access_config {
           
           }
