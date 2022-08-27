e4d8ed7b56b75f7dd126d769e6a99fea18effd24
testingcloud1994 <real.suraj1234@gmail.com>
2022-08-27 16:54:40 +0530

    change 3 instance

--- cerateinstance.tf
+++ cerateinstance.tf
@@ -1,10 +1,13 @@
 resource "google_compute_instance" "vm_instance" {
+    count = 3
     name = "terraform_instance"
     machine_type = "e2-micro"
+    tags = "threedemoinstance"
     boot_disk {
       initialize_params{
         image= "ubuntu-os-cloud/ubuntu-2004-lts"
       }
+    
       }
 network_interface {
     network= "default"
