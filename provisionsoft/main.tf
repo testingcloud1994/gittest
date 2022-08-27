bdca456ed1b0946c52402e97f2f2acb9b74f85bd
testingcloud1994 <real.suraj1234@gmail.com>
2022-08-27 21:21:50 +0530

    dcdc

--- provisionsoft/main.tf
+++ provisionsoft/main.tf
@@ -1,23 +1,24 @@
-resource "google_compute_project_metadata_item" "keyvalue" {
-  metadata={
-      ssh-keys= file("${var.keyvalue.publickey}")
-  }
-  }
-
 resource "google_compute_instance" "vm_instance" {
     count = 1
     name = "${var.vmname}"
     machine_type = "e2-micro"
+    
+    
+    
     boot_disk {
       initialize_params{
         image= "ubuntu-os-cloud/ubuntu-2004-lts"
       }
           }
+    metadata = {
+      "key" = "file(${var.pathtopublickey})"
+    }
+    metadata_startup_script ="apt-get update && apt-get install nginx"
         
-network_interface {
-    network= "default"
-    access_config {
-     nat_ip="${var.securitygp[0]}"
+    network_interface {
+      network= "default"
+      access_config {
+      nat_ip="${var.securitygp[0]}"
                   }}
   
 }
