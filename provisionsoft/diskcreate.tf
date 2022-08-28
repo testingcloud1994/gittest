501eab3d0fc7444b8828b0ce9fe8a80745bdd023
testingcloud1994 <real.suraj1234@gmail.com>
2022-08-28 19:59:38 +0530

    kj

--- provisionsoft/diskcreate.tf
+++ provisionsoft/diskcreate.tf
@@ -1,6 +1,6 @@
 resource "google_compute_disk" "testme0diskcerate" {
     name = "testme-diskcerate"
-    size = "80Gb"
+    size = "80GB"
     type = "pd-ssd"
     zone = "${var.myzone}"
     physical_block_size_bytes = 4096
