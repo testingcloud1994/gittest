a3eff03d0186b6aff0c6e518249192523217ba1d
testingcloud1994 <real.suraj1234@gmail.com>
2022-08-28 20:00:24 +0530

    my project changes

--- provisionsoft/diskcreate.tf
+++ provisionsoft/diskcreate.tf
@@ -1,6 +1,6 @@
 resource "google_compute_disk" "testme0diskcerate" {
     name = "testme-diskcerate"
-    size = "80GB"
+    size =  80GB
     type = "pd-ssd"
     zone = "${var.myzone}"
     physical_block_size_bytes = 4096
