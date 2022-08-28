479b41a25a5c2fddc518123f84b179fc90339368
testingcloud1994 <real.suraj1234@gmail.com>
2022-08-28 19:17:50 +0530

    sdsds

--- provisionsoft/network.tf
+++ provisionsoft/network.tf
@@ -8,5 +8,5 @@ resource "google_compute_subnetwork" "testme-subnet" {
     name = "testme-subnet"
     ip_cidr_range = "192.168.1.0/22"
     region = "${var.myregion}"
-    network = google_compute_network.testme-network 
+    network = "${google_compute_network.testme-network}" 
 }
