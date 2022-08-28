fbc35d85683835093f72db9d937e0e3798c78d89
testingcloud1994 <real.suraj1234@gmail.com>
2022-08-28 18:52:02 +0530

    jjh

--- provisionsoft/main.tf
+++ provisionsoft/main.tf
@@ -7,7 +7,14 @@ resource "google_compute_instance" "vm_instance" {
         image= "ubuntu-os-cloud/ubuntu-2004-lts"
       }
           }
-    metadata_startup_script ="ss.sh"
+    metadata_startup_script =<<SCRIPT
+        apt-get -y update
+        apt-get -y install nginx
+        export HOSTNAME=$(hostname | tr -d '\n')
+        export PRIVATE_IP=$(curl -sf -H 'Metadata-Flavor:Google' http://metadata/computeMetadata/v1/instance/network-interfaces/0/ip | tr -d '\n')
+        echo "Welcome to $HOSTNAME - $PRIVATE_IP" > /usr/share/nginx/www/index.html
+        service nginx start
+        SCRIPT
     network_interface {
       network= "default"
       access_config {
