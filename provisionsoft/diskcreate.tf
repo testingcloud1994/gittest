9fc76921e474703577ce8b83a724e5558f67c5e8
testingcloud1994 <real.suraj1234@gmail.com>
2022-08-28 20:14:43 +0530

    ssss

--- provisionsoft/diskcreate.tf
+++ provisionsoft/diskcreate.tf
@@ -3,6 +3,7 @@ resource "google_compute_disk" "testme0diskcerate" {
     size =  80
     type = "pd-ssd"
     zone = "${var.myzone}"
+    image = "ubuntu-os-cloud/ubuntu-2004-lts"
     physical_block_size_bytes = 4096
    
   
--- provisionsoft/main.tf
+++ provisionsoft/main.tf
@@ -2,11 +2,11 @@ resource "google_compute_instance" "vm_instance" {
     name = "${var.vmname}"
     machine_type = "e2-micro"
        
-    boot_disk {
+    /*boot_disk {
       initialize_params{
         image= "ubuntu-os-cloud/ubuntu-2004-lts"
       }
-          }
+          } */
     network_interface {
       subnetwork = google_compute_subnetwork.testme-subnet.name
       access_config {
